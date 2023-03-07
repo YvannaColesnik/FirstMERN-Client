45d133e668728d2323335fd1486638b692f26047
YvannaColesnik <ycolesnik@gmail.com>
2023-03-06 23:12:49 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [friends, setFriends] = useState([]);
+
+  const addFriend = () => {
+    axios.post('http://localhost:3000/addFriend',
+      {
+        name: name, age: age
+      })
+      .then(() => {
+        setFriends([...friends, {name: name, age: age}]);
+      }).catch(() => {
+        // alert('oh no');
+      });
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:3000/read')
+      .then((response) => {
+        setFriends(response.data)
+      }).catch(() => {
+        alert('oh no');
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputs">
+        <input type="text" placeholder='Friend name' onChange={(event) => {
+          setName(event.target.value);
+        }}
+        />
+        <input type="number" placeholder='Friend age' onChange={(event) => {
+          setAge(event.target.value);
+        }}
+        />
+
+        <button onClick={addFriend}> Add friend</button>
+      </div>
+
+      <div className='lisfOfFriends'>
+        {friends.map((val) => {
+          return (
+            <div className='friend'>
+              <h3>NAME: {val.name}</h3> 
+              <h3>AGE: {val.age}</h3> 
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
